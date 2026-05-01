62c662799b6e7ad553af0be0646ab9b5a7e32549
GerdaMGit <gerda0515@gmail.com>
2026-05-01 11:49:26 +0300

    4

--- Assets/Scripts/Models/Achievement.cs
+++ Assets/Scripts/Models/Achievement.cs
@@ -24,8 +24,6 @@ namespace SupabaseModels
         [Column("fk_userid")]
         public string UserId { get; set; }
 
-        //For UI
-        public bool Unlocked { get; set; }
-        public bool Claimed { get; set; }
+        
     }
 }
